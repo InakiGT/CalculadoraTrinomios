3e801faa79b56799169e94272aaf08d4fa3f22d8
Iñaki García <yarimfuego@gmail.com>
2022-08-04 16:28:27 -0500

    Fisrt commit

---
+++ src/completar-trinomio.cc
@@ -0,0 +1,9 @@
+#include "../include/trinomios";
+
+void CompletarTrinomio(int a, int b, int c, int array[]) {
+    int d = (b / 2) * (b / 2);
+
+    array[0] = a;
+    array[1] = b;
+    array[1] = d;
+}
